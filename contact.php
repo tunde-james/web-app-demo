cc96accd6c216e3fe056822a0eec5d450e27562d
Tunde James <tundejames04@gmail.com>
2024-05-21 10:41:07 +0100

    Implement dynamic routing for each page using super globals

--- contact.php
+++ contact.php
@@ -1,4 +1,6 @@
 <?php
+
+require 'functions.php';
 	
 $heading = "Contact Us";
 
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -8,9 +8,11 @@
 				<div class="hidden md:block">
 					<div class="ml-10 flex items-baseline space-x-4">
 						<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-						<a href="/" class="text-gray-300 hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-						<a href="/about.php" class="bg-gray-900 text-white hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
-						<a href="/contact.php" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+						<a href="/" class="<?= urls('/') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">
+							Home
+						</a>
+						<a href="/about.php" class="<?= urls('/about.php') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+						<a href="/contact.php" class="<?= urls('/contact.php') ? 'bg-gray-900 text-white' : 'text-gray-300' ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
 					</div>
 				</div>
 			</div>
@@ -23,7 +25,7 @@
 							<path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
 						</svg>
 					</button>
-					
+
 					<!-- Profile dropdown -->
 					<div class="relative ml-3">
 						<div>
@@ -33,7 +35,7 @@
 								<img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
 							</button>
 						</div>
-						
+
 						<!--
 						  Dropdown menu, show/hide based on menu state.
 		  
@@ -70,7 +72,7 @@
 			</div>
 		</div>
 	</div>
-	
+
 	<!-- Mobile menu, show/hide based on menu state. -->
 	<div class="md:hidden" id="mobile-menu">
 		<div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
@@ -99,9 +101,11 @@
 				</button>
 			</div>
 			<div class="mt-3 space-y-1 px-2">
-				<a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your Profile</a>
+				<a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your
+					Profile</a>
 				<a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
-				<a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign out</a>
+				<a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign
+					out</a>
 			</div>
 		</div>
 	</div>
