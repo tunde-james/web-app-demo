97c2c440caa2affb98bd27f1398385bb1db29136
Tunde James <tundejames04@gmail.com>
2024-05-21 21:55:50 +0100

    Connect to the database using PDO

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+// connect to MySQL database
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;password=demo;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo "<li>" . $post['title'] . "</li>";
+}
