ac113cca88910c6bbefcb5b55dafcea3a564a96f
Tunde James <tundejames04@gmail.com>
2024-05-22 15:30:49 +0100

    Refactor: setup the database connection to be dynamic

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require 'functions.php';
 // require 'router.php';
 require 'Database.php';
 
+$config = require('config.php');
 
-
-$db = new Database();
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+$posts = $db->query("select * from posts")->fetchAll();
 
 dd($posts);
