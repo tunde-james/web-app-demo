3e03b7ae5a4a99f565f87fb7253a247342d04c03
Tunde James <tundejames04@gmail.com>
2024-05-22 16:10:37 +0100

    Refactor: Change the sql query to avoid sql injection

--- index.php
+++ index.php
@@ -5,8 +5,13 @@ require 'functions.php';
 require 'Database.php';
 
 $config = require('config.php');
-
 $db = new Database($config['database']);
-$posts = $db->query("select * from posts")->fetchAll();
+
+$id = ($_GET['id']);
+$query = "select * from posts where id = ?";
+
+// dd($query);
+
+$posts = $db->query($query, [$id])->fetch();
 
 dd($posts);
