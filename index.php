615dd6b8efecd7c84b39333f5acd808c220988a7
Tunde James <tundejames04@gmail.com>
2024-05-27 10:20:55 +0100

    Render notes Fetch all notes Fetch notes by id

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
 
 require 'functions.php';
-// require 'router.php';
 require 'Database.php';
-
-$config = require('config.php');
-$db = new Database($config['database']);
-
-$id = ($_GET['id']);
-$query = "select * from posts where id = ?";
-
-// dd($query);
-
-$posts = $db->query($query, [$id])->fetch();
-
-dd($posts);
+require 'router.php';
