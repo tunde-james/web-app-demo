e11bde2323ac3f79951207aa4638401e15f2220a
Tunde James <tundejames04@gmail.com>
2024-05-28 15:25:55 +0100

    Extract form validation to a simple form validation class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,20 +1,21 @@
 <?php
 
-$config = require('config.php');
+require 'Validator.php';
+
+$config = require 'config.php';
 $db = new Database($config['database']);
+
+dd(Validator::email('tude@example.com'));
 
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $errors = [];
 
-  if (strlen($_POST['body']) === 0) {
-    $errors['body'] = 'A body is required';
+  if (!Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1000 characters is required';
   }
 
-  if (strlen($_POST['body']) > 1000) {
-    $errors['body'] = 'The body cannot be more than 1000 characters';
-  }
 
   if (empty($errors)) {
     $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
