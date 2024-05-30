4b9d6af2292e5b08ee0e970dedc0e5082977c68d
Tunde James <tundejames04@gmail.com>
2024-05-30 10:25:36 +0100

    File Organisation: Autloading and extraction

--- views/about.view.php
+++ views/about.view.php
@@ -1,11 +1,11 @@
-<?php require("partials/head.php") ?>
-<?php require("partials/nav.php"); ?>
-<?php require("partials/banner.php") ?>
-	
-	<main>
-		<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-			<p>Now you are on the About page.</p>
-		</div>
-	</main>
-	
-<?php require("partials/footer.php") ?>
+<?php require base_path("views/partials/head.php") ?>
+<?php require base_path("views/partials/nav.php"); ?>
+<?php require base_path("views/partials/banner.php") ?>
+
+<main>
+	<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+		<p>Now you are on the About page.</p>
+	</div>
+</main>
+
+<?php require base_path("views/partials/footer.php") ?>
