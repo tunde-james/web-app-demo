759bc59bbbd951a3ddc606a948a39e7a4b8d1012
Tunde James <tundejames04@gmail.com>
2024-05-30 12:13:17 +0100

    Implement the delete method from the show controller

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -7,7 +7,13 @@
 		<p class="mb-6">
 			<a href="/notes" class="text-blue-500 underline">go back...</a>
 		</p>
+
 		<p><?= htmlspecialchars($note['body']) ?></p>
+
+		<form method="POST" class="mt-4">
+			<input type="hidden" name="id" value="<?= $note['id'] ?>">
+			<button class="text-sm text-red-500">Delete</button>
+		</form>
 	</div>
 </main>
 
