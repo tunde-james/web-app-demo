a9fa85293d9cb9d7a239c6a6c339663e9d2cef24
Tunde James <tundejames04@gmail.com>
2024-05-31 22:10:18 +0100

    Build a better PHP router

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -11,6 +11,7 @@
 		<p><?= htmlspecialchars($note['body']) ?></p>
 
 		<form method="POST" class="mt-4">
+			<input type="hidden" name="_method" value="DELETE">
 			<input type="hidden" name="id" value="<?= $note['id'] ?>">
 			<button class="text-sm text-red-500">Delete</button>
 		</form>
