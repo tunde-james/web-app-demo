ef9ea36d52328cb19b0693e13dfc9408256e6482
Tunde James <tundejames04@gmail.com>
2024-06-04 15:29:34 +0100

    Create a middleware

--- views/session/create.view.php
+++ views/session/create.view.php
@@ -6,16 +6,14 @@
     <div class="w-full max-w-md space-y-8">
       <div>
         <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-          Register for a new account
-        </h2>
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Log In!</h2>
       </div>
 
-      <form class="mt-8 space-y-6" action="/register" method="POST">
+      <form class="mt-8 space-y-6" action="/session" method="POST">
         <div class="-space-y-px rounded-md shadow-sm">
           <div>
             <label for="email" class="sr-only">Email address</label>
-            <input id="email" name="email" type="email" autocomplete="email" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address">
+            <input id="email" name="email" type="email" autocomplete="email" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" value="<?= old('email') ?>">
           </div>
 
           <div>
@@ -26,7 +24,7 @@
 
         <div>
           <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-            Register
+            Log In
           </button>
         </div>
 
