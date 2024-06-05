c287c944ccead5ca83b5cdf1108bd558f302f570
Tunde James <tundejames04@gmail.com>
2024-06-05 09:18:01 +0100

    Extract form validation object

--- Http/controllers/session/store.php
+++ Http/controllers/session/store.php
@@ -2,29 +2,20 @@
 
 use Core\App;
 use Core\Database;
-use Core\Validator;
+use Http\Forms\LoginForm;
 
 $db = App::resolve(Database::class);
 
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// validate form inputs
-$errors = [];
+$form = new LoginForm();
 
-if (!Validator::email($email)) {
-  $errors['email'] = 'Please provide a valid email address.';
-}
-
-if (!Validator::string($password)) {
-  $errors['password'] = 'Please provide a valid password.';
-}
-
-if (!empty($errors)) {
+if (!$form->validate($email, $password)) {
   return view('session/create.view.php', [
-    'errors' => $errors
+    'errors' => $form->errors()
   ]);
-}
+};
 
 // match the credentials
 $user = $db->query('select * from users where email = :email', [
